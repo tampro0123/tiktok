bc3aaaab47342ddd97f11c962051a9e3d8991842
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2023-01-13 21:34:29 +0700

    Add Default layout frame

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,35 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss'
+import images from '~/assets/images';
+
 
 const cx = classNames.bind(styles)
 function Header() {
   return <header className={cx('wrapper')}>
 
     <div className={cx('inner')}>
+      <img src={images.logo} alt="TikTok" />
+
+      <div className={cx('search')}>
+        <input
+          placeholder='Search account or video!'
+          spellCheck="false"
+
+        ></input>
+        <button className={cx('clear')}>
+
+          <FontAwesomeIcon icon={faCircleXmark} />
+        </button>
+        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+        <button className={cx('search-btn')}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+
+        </button>
+      </div>
+      <div className={cx('actions')}> </div>
 
     </div>
 
